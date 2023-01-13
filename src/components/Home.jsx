1bd4abb845a095043fe3c131335f210ca3a9b01e
Kvz666 <55993619+Kvz666@users.noreply.github.com>
2023-01-13 17:18:59 +0100

    navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import NavBar from "./Navbar";
-
+import Navbar from "./navbar/Navbar";
 export default function Home() {
-    return (
-        <div className="homepage">
-            <div className="mid-square-text">
-                <h3>Reshaping Businesses Through</h3>
-                <h2>We provide Web aplications</h2>
-                <p>Edge out your competition with our unique and innovative digital product development solutions.</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="homepage">
+      <div className="homepage-navbar">
+        <Navbar />
+      </div>
+      <div className="mid-square-text">
+        <h3>Reshaping Businesses Through</h3>
+        <h2>We provide Web aplications</h2>
+        <p>
+          Edge out your competition with our unique and innovative digital
+          product development solutions.
+        </p>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Navbar.css";
+import Logo from "../images/img1.png";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      {/*      <!-- LOGO --> */}
+      <div className="logo">
+        <img src={Logo} alt="logo.png" />
+        <h1>ISCloud</h1>
+      </div>
+      {/*      <!-- NAVIGATION MENU --> */}
+
+      <ul className="nav-links">
+        {/*        <!-- USING CHECKBOX HACK --> */}
+
+        <input type="checkbox" id="checkbox_toggle" />
+
+        <label for="checkbox_toggle" className="hamburger">
+          &#9776;
+        </label>
+        {/*        <!-- NAVIGATION MENUS --> */}
+        <div className="menu">
+          <li>
+            <h2>About Us</h2>
+          </li>
+
+          <li>
+            <h2>Contact Us</h2>
+          </li>
+
+          <li>
+            <h2>EN/DE </h2>
+          </li>
+        </div>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
