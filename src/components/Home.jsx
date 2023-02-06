67beba7760f8e74a1ea5cd208bcf674da4141e74
Kvz666 <55993619+Kvz666@users.noreply.github.com>
2023-02-06 17:51:48 +0100

    res

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -58,7 +58,10 @@ export default function Home() {
           <Info />
         </>
 
-        <> {/* <WebDevProcess /> */}</>
+        <>
+          {" "}
+          <WebDevProcess />
+        </>
         <div className="about" ref={ref}>
           {" "}
           <About />
